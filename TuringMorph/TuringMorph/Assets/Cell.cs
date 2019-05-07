825dc83c641f40f9bfc2d4dc9d3cf0c676b65e40
Freddy Martínez <fhmartinezq@unal.edu.co>
2019-05-07 00:58:06 -0500

    Inicio proyecto Turing Morph

---
+++ TuringMorph/TuringMorph/Assets/Cell.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+    public float a = 1.0f;
+    public float b = 0f;
+}
---
+++ TuringMorph/TuringMorph/Assets/ReactionDiffusionGenerator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactionDiffusionGenerator : MonoBehaviour {
+    public int width = 256;
+    public int height = 256;
+
+    private Cell[,] grid;
+    private Texture2D texture;
+
+	// Use this for initialization
+	void Start () {
+        InitializeTexture();
+        InitializeGrid();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RefreshTexture();
+    }
+
+    void InitializeTexture()
+    {
+        texture = new Texture2D(width, height);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = texture;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                texture.SetPixel(x, y, Color.cyan);
+            }
+        }
+        texture.Apply();
+    }
+
+    void InitializeGrid()
+    {
+        grid = new Cell[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = new Cell();
+            }
+        }
+
+        for (int x = 100; x < 110; x++)
+        {
+            for (int y = 100; y < 110; y++)
+            {
+                grid[x, y].b = 1f;
+            }
+        }
+    }
+
+    void RefreshTexture()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                texture.SetPixel(x, y, Color.Lerp(Color.white, Color.black, grid[x, y].b));
+            }
+        }
+        texture.Apply();
+    }
+}
