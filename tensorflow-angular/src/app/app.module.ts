0bef1e91b8f9a98633a1a9800edd21a10e4cee72
Freddy Hernán Martínez Quiñones <fmartinez@belatrix.com>
2019-11-15 17:05:06 -0500

    Minor adjust principal component, include bootstrap

--- tensorflow-angular/src/app/app.module.ts
+++ tensorflow-angular/src/app/app.module.ts
@@ -4,9 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LinnearRegressionComponent, PolinomialRegressionComponent } from './components/';
-import { PerceptronComponent } from './components/perceptron/perceptron.component';
-import { OverfittingComponent } from './components/overfitting/overfitting.component';
+import { LinnearRegressionComponent, PolinomialRegressionComponent, PerceptronComponent, OverfittingComponent } from './components/';
 
 @NgModule({
   declarations: [
